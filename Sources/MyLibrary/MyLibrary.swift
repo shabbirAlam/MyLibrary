a0ca924ea2a071e30b04dc972345ee0c9269e1d7
Shabbir <mdshabbiralam007@gmail.com>
2026-05-24 11:01:57 +0530

    - added helper method.

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public final class MyLibraryHelper {
+    public var greeting: String {
+        return "Hello, World!"
+    }
+}
